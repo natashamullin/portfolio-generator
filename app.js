a92d3d979222310cb50cc0531effeff4bd856d9a
Natasha Mullin <69641107+natashamullin@users.noreply.github.com>
2020-11-13 16:19:14 -0800

    added more stuff

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const { truncate } = require('fs');
 const inquirer = require('inquirer');
 
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -47,11 +51,6 @@ const promptUser = () => {
                 }
             }
         },
-        {
-            type: 'input',
-            name: 'about',
-            message: 'Provide some information about yourself:'
-        }
     ]);
 };
 
@@ -61,13 +60,8 @@ const promptUser = () => {
 //     .then(projectAnswers => console.log(projectAnswers));
 
 const promptProject = portfolioData => {
-    console.log(`
-  =================
-  Add a New Project
-  =================
-  `);
-    if (!portfolioData.Projects) {
-        portfolioData.Projects = [];
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
     }
     return inquirer.prompt([
 
@@ -132,32 +126,89 @@ const promptProject = portfolioData => {
     ]);
 };
 
+// const mockData = {
+//     name: 'Lernantino',
+//     github: 'lernantino',
+//     confirmAbout: true,
+//     about:
+//         'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
+//     projects: [
+//         {
+//             name: 'Run Buddy',
+//             description:
+//                 'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+//             languages: ['HTML', 'CSS'],
+//             link: 'https://github.com/lernantino/run-buddy',
+//             feature: true,
+//             confirmAddProject: true
+//         },
+//         {
+//             name: 'Taskinator',
+//             description:
+//                 'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+//             languages: ['JavaScript', 'HTML', 'CSS'],
+//             link: 'https://github.com/lernantino/taskinator',
+//             feature: true,
+//             confirmAddProject: true
+//         },
+//         {
+//             name: 'Taskmaster Pro',
+//             description:
+//                 'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+//             languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+//             link: 'https://github.com/lernantino/taskmaster-pro',
+//             feature: false,
+//             confirmAddProject: true
+//         },
+//         {
+//             name: 'Robot Gladiators',
+//             description:
+//                 'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
+//             languages: ['JavaScript'],
+//             link: 'https://github.com/lernantino/robot-gladiators',
+//             feature: false,
+//             confirmAddProject: false
+//         }
+//     ]
+// };
+
+
+// const pageHTML = generatePage(mockData);
 
 promptUser()
     .then(answer => console.log(answer))
     .then(promptProject)
     .then(projectAnswers => console.log(projectAnswers))
-    .then(projectData => {
-        portfolioData.projects.push(projectData)
-        if (projectData.confirmAddProject) {
-            return promptProject(portfolioData);
-        }
-        else {
-            return portfolioData;
-        }
+    .then(portfolioData => {
+        console.log(portfolioData)
+            .then(projectData => {
+                portfolioData.projects.push(projectData)
+                if (projectData.confirmAddProject) {
+                    return promptProject(portfolioData);
+                }
+                else {
+                    return portfolioData;
+                }
+            });
+        //         const pageHTML = generatePage(portfolioData);
+
+        //         fs.writeFile('./index.html', pageHTML, err => {
+        //             if (err) throw new Error(err);
+
+        //             //   console.log('Page created! Check out index.html in this directory to see it!');
+        //             // });
+
+        //            
+
+
+
+
+
+
+        // const pageHTML = generatePage(name, github);
+
+        // fs.writeFile('./index.html', pageHTML, err => {
+        //     if (err) throw new Error(err);
+
+        //     console.log('portfolio complete! check out index.html to see the output!')
     });
-
-
-
-
-// const fs = require('fs');
-
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw new Error(err);
-
-//     console.log('portfolio complete! check out index.html to see the output!')
-// });
